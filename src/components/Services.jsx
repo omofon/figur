f6037b346b194599bbcf35b7c9179fc7b25fd6c4
omofon <abasiomofonudoh@gmail.com>
2026-03-02 22:27:47 +0100

    fix: update service card transition

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,15 +15,15 @@ const Services = () => {
         {/* Service 1: Virtual Dollar Card */}
         <div className="w-full relative overflow-hidden bg-[#F1E8FF] border border-[#EFE7FF] rounded-3xl flex flex-col lg:flex-row group">
           <div className="flex flex-col gap-3 px-8 lg:px-12 py-16">
-            <span className="text-xs font-semibold text-primary-navy">
-              Virtual Dollar Card
-            </span>
-            <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
-              Shop locally & Pay globally with Figur USD Card
-            </h3>
-            <button className="mt-2 w-fit px-4 py-2 bg-primary-blue rounded-full text-white text-sm hover:bg-primary-navy cursor-pointer transition-all opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:translate-y-0 lg:translate-y-2">
-              Create a Card
-            </button>
+            <div className="flex flex-col gap-3 transition-sleek">
+              <span className="text-xs font-semibold text-primary-navy">
+                Virtual Dollar Card
+              </span>
+              <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
+                Shop locally & Pay globally with Figur USD Card
+              </h3>
+            </div>
+            <button className="service-button">Create a Card</button>
           </div>
           <div className="w-full h-full overflow-hidden self-center flex justify-center">
             <img
@@ -35,7 +35,7 @@ const Services = () => {
         </div>
 
         {/* Service 2A: Mobile Dashboard Image */}
-        <div className="w-full lg:flex-1 rounded-3xl relative overflow-hidden bg-[#ff9012] border border-[#F28911] flex items-end justify-center min-h-[300px]">
+        <div className="w-full lg:flex-1 rounded-3xl relative overflow-hidden bg-[#ff9012] border border-[#F28911] flex items-end justify-center min-h-75">
           <img
             src="/images/mobile-app-dashoard.png"
             alt="Mobile dashboard"
@@ -45,29 +45,29 @@ const Services = () => {
 
         {/* Service 2B: Airtime to Cash */}
         <div className="w-full lg:flex-[2_2_0%] bg-[#FFF9ED] border border-[#DAEFF0] rounded-3xl flex flex-col justify-center gap-3 px-8 lg:px-12 py-16 group">
-          <span className="text-xs font-semibold text-primary-navy">
-            Airtime to Cash
-          </span>
-          <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
-            Convert your excess Airtime to Cash Instantly! 😊
-          </h3>
-          <button className="mt-2 w-fit px-4 py-2 bg-primary-blue rounded-full text-white text-sm hover:bg-primary-navy cursor-pointer transition-all opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:translate-y-0 lg:translate-y-2">
-            Convert Airtime to Cash
-          </button>
+          <div className="flex flex-col gap-3 transition-sleek">
+            <span className="text-xs font-semibold text-primary-navy">
+              Airtime to Cash
+            </span>
+            <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
+              Convert your excess Airtime to Cash Instantly! 😊
+            </h3>
+          </div>
+          <button className="service-button">Convert Airtime to Cash</button>
         </div>
 
         {/* Service 3: Gift Card */}
         <div className="w-full relative overflow-hidden bg-[#E4F4F5] border border-[#DAEFF0] rounded-3xl flex flex-col lg:flex-row group">
           <div className="flex flex-col gap-3 px-8 lg:px-12 py-16 lg:w-1/2">
-            <span className="text-xs font-semibold text-primary-navy">
-              Gift Card
-            </span>
-            <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
-              Purchase over 5,000+ gift cards at the best rates.
-            </h3>
-            <button className="mt-2 w-fit px-4 py-2 bg-primary-blue rounded-full text-white text-sm hover:bg-primary-navy cursor-pointer transition-all opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:translate-y-0 lg:translate-y-2">
-              Purchase Gift Card
-            </button>
+            <div className="flex flex-col gap-3 transition-sleek">
+              <span className="text-xs font-semibold text-primary-navy">
+                Gift Card
+              </span>
+              <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
+                Purchase over 5,000+ gift cards at the best rates.
+              </h3>
+            </div>
+            <button className="service-button">Purchase Gift Card</button>
           </div>
           <div className="w-full lg:w-1/2 flex justify-end items-end">
             <img
@@ -80,15 +80,15 @@ const Services = () => {
 
         {/* Service 4A: Pay Bills */}
         <div className="w-full lg:flex-[2_2_0%] bg-[#E4F4F5] border border-[#DAEFF0] rounded-3xl flex flex-col justify-center gap-3 px-8 lg:px-12 py-16 group">
-          <span className="text-xs font-semibold text-primary-navy">
-            Pay Bills
-          </span>
-          <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
-            Buy Cheap Data, Electricity, TV, Airtime & more
-          </h3>
-          <button className="mt-2 w-fit px-4 py-2 bg-primary-blue rounded-full text-white text-sm hover:bg-primary-navy cursor-pointer transition-all opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:translate-y-0 lg:translate-y-2">
-            Get Started
-          </button>
+          <div className="flex flex-col gap-3 transition-sleek">
+            <span className="text-xs font-semibold text-primary-navy">
+              Pay Bills
+            </span>
+            <h3 className="text-3xl md:text-4xl font-extrabold text-primary-navy leading-tight">
+              Buy Cheap Data, Electricity, TV, Airtime & more
+            </h3>
+          </div>
+          <button className="service-button">Get Started</button>
         </div>
 
         {/* Service 4B: Woman with Phone Image */}
