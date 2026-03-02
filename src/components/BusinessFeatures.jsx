76a24a5c978abe7f04f4aad5f46fef2ca37cb944
omofon <abasiomofonudoh@gmail.com>
2026-03-02 22:40:25 +0100

    feat: Add top-margin to features grid

--- src/components/BusinessFeatures.jsx
+++ src/components/BusinessFeatures.jsx
@@ -67,7 +67,7 @@ const BusinessFeatures = () => {
         </div>
 
         {/* Features Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-15">
           {features.map((feature, index) => (
             <div
               key={index}
